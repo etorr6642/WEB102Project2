cac86b3fb067af2f02d1a8b8824de4261479a944
Edward Torres <etorres5124@gmail.com>
2025-06-15 23:35:50 -0700

    added images to answers; setTimeout for transition

--- Flashcards/src/App.jsx
+++ Flashcards/src/App.jsx
@@ -1,20 +1,105 @@
 import './App.css';
 import React, { useState } from 'react';
-
+//question about the first 151 pokemon
 const questions = [
   {
-    question: "What is the name of the first Pokemon?",
+    question: "What is the very first Pokémon in the Pokédex?",
     answer: "Bulbasaur",
+    image: "/bulb.jpg"
   },
   {
-    question: "What is the name of the second Pokemon?",
-    answer: "Ivysaur",
+    question: "Which Pokémon evolves into Charizard?",
+    answer: "Charmeleon",
+    image: "/charmeleon.jpg"
   },
   {
-    question: "What is the name of the third Pokemon?",
-    answer: "Venusaur",
+    question: "Which three Pokémon can evolve from Eevee in Gen 1?",
+    answer: "Vaporeon, Jolteon, Flareon",
+    image: "three.jpg"
   },
-]
+  {
+    question: "What type is Pikachu?",
+    answer: "Electric",
+    image: "/pika.jpg"
+  },
+  {
+    question: "Which legendary bird is Ice/Flying-type?",
+    answer: "Articuno",
+    image: "/arti.png"
+  },
+  {
+    question: "Which Pokémon has a skull on its head as part of its body?",
+    answer: "Cubone",
+    image: "/cubone.png"
+  },
+  {
+    question: "What are the three final evolutions of the Kanto starter Pokémon?",
+    answer: "Venusaur, Charizard, Blastoise",
+    image: "/vcb.jpg"
+  },
+  {
+    question: "Which Pokémon is number 151 in the Pokédex?",
+    answer: "Mew",
+    image: "/mew.png"
+  },
+  {
+    question: "Which Pokémon evolves into Gyarados?",
+    answer: "Magikarp",
+    image: "/magikarp.jpg"
+  },
+  {
+    question: "Which two Pokémon are fossil Pokémon in Gen 1?",
+    answer: "Omanyte and Kabuto",
+    image: "/ok.png"
+  },
+  {
+    question: "Which Psychic-type Pokémon is known for its spoons?",
+    answer: "Alakazam",
+    image: "/alakazam.jpg"
+  },
+  {
+    question: "What is the only Ghost-type evolutionary line in Gen 1?",
+    answer: "Gastly → Haunter → Gengar",
+    image: "/ghg.jpg"
+  },
+  {
+    question: "Which Pokémon is known for saying its own name and is a balloon?",
+    answer: "Jigglypuff",
+    image: "/jigglypuff.jpg"
+  },
+  {
+    question: "Which Pokémon is the original form of Raichu?",
+    answer: "Pikachu",
+    image: "/pika.jpg"
+  },
+  {
+    question: "Which Pokémon do Team Rocket often use in the anime?",
+    answer: "Ekans and Koffing",
+    image: "/ke.jpg"
+  },
+  {
+    question: "Which two Pokémon can only evolve via trading in Gen 1?",
+    answer: "Machoke Kadabra Haunter Graveler",
+    image: "/four.jpg"
+  },
+  {
+    question: "Which Pokémon is known as the 'Evolution Pokémon'?",
+    answer: "Eevee",
+    image: "/eevee.jpg"
+  },
+  {
+    question: "Which dual-type Bug/Flying Pokémon evolves from Caterpie?",
+    answer: "Butterfree",
+    image: "/butterfree.png"
+  },
+  {
+    question: "Which Dragon-type Pokémon is the final evolution of Dratini?",
+    answer: "Dragonite",
+    image: "/dragonite.jpg"
+  }
+];
+
+
 
 const App = () => {
   // State variables
@@ -26,6 +111,7 @@ const App = () => {
   const flipCard = () => {
     setShowAnswer(!showAnswer);
   }
+
 
   return (
     <div className="App">
@@ -51,25 +137,34 @@ const App = () => {
             <h3>{questions[currentQuestion].question}</h3>
           </div>
           <div className="back card">
-            <h3>{questions[currentQuestion].answer}</h3>
+            <div className="answer">
+              <h3>{questions[currentQuestion].answer}</h3>
+              <img src={questions[currentQuestion].image} alt={questions[currentQuestion].answer} />
+            </div>
           </div>
         </div>
       </div>
 
       <div className='nextPrevious'>
-        {/* button to go to the next question */}
-        <button className="next" onClick={() => {
-          setCurrentQuestion((currentQuestion + 1) % questions.length);
-          setShowAnswer(false);
-        }}>
-          Next Question
-        </button>
-        {/* button to go to the previous question */}
+         {/* button to go to the previous question */}
         <button className="prev" onClick={() => {
-          setCurrentQuestion((currentQuestion - 1 + questions.length) % questions.length);
           setShowAnswer(false);
+          setTimeout(() => {
+            setCurrentQuestion((currentQuestion - 1 + questions.length) % questions.length);
+          },200)
+          
         }}>
           Previous Question
+        </button>
+        {/* button to go to the next question */}
+        <button className="next" onClick={() => {
+          setShowAnswer(false);
+          setTimeout(() => {
+          setCurrentQuestion((currentQuestion + 1) % questions.length);
+          }, 200)
+          
+        }}>
+          Next Question
         </button>
       </div>
 
--- README.md
+++ README.md
@@ -26,8 +26,8 @@ The following **required** functionality is completed:
 
 The following **optional** features are implemented:
 
-- [ ] Cards contain images in addition to or in place of text
-  - [ ] Some or all cards have images in place of or in addition to text
+- [x] Cards contain images in addition to or in place of text
+  - [x] Some or all cards have images in place of or in addition to text
 - [ ] Cards have different visual styles such as color based on their category
   - Example categories you can use:
     - Difficulty: Easy/medium/hard
